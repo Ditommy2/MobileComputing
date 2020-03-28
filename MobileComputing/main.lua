518e4fac9f26fd3a6b64a73d8a0cc5c9444cefa3
RBraga-droid <62329743+RBraga-droid@users.noreply.github.com>
2020-03-28 15:10:47 +0100

    prep

--- MobileComputing/main.lua
+++ MobileComputing/main.lua
@@ -5,3 +5,9 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+local composer = require("composer")
+display.setStatusBar(display.HiddenStatusBar)
+math.randomseed(os.time())
+composer.gotoScene("nuova")
+audio.reserveChannels(1)
+audio.setVolume( 0.1, {channel=1} )
---
+++ MobileComputing/nuova.lua
@@ -0,0 +1,120 @@
+local composer = require( "composer" )
+local mysql=require("luasql.mysql")
+local scene = composer.newScene()
+local widget = require("widget")
+local Username
+local password
+--local function handleButtonEvent( event )
+  --  if ( "ended" == event.phase ) then
+  --  	local URL = "http://web.web.com/yourscript.php?username=" .. urlencode( username.text ) .. "&password=" .. urlencode(password.text)
+--network.request(URL, "GET", networkListener)
+
+
+--	end
+--end
+
+
+local function handleButtonEvent( event )
+    if ( "ended" == event.phase ) then
+      local file= sqlite3.open(  "192.168.1.67:5432/Program Files/PostgreSQL/12/data/base/33004")
+    --  local query= "INSERT INTO Mammita VALUES(1," .. (username.text) ..", ".. (password.text) ");"
+    local query= "INSERT INTO Mammita VALUES(1, PROVA, 123);"
+      file:exec( query )
+	end
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
+
+
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+
+-- create()
+function scene:create( event )
+
+	local sceneGroup = self.view
+	-- Code here runs when the scene is first created but has not yet appeared on screen
+
+ username = native.newTextField( 160, 200, 180, 30 )
+username.placeholder = "Username"
+sceneGroup:insert(username)
+
+ password = native.newTextField( 160, 260,180, 30 )
+password.isSecure = true
+password.placeholder = "Password"
+sceneGroup:insert(password)
+
+local Button = widget.newButton(
+   {
+       shape = "roundedRect",
+       left = 70,
+       top = 360,
+       id = "Nuova",
+       label = "Nuova Partita",
+       onEvent = handleButtonEvent
+   }
+)
+sceneGroup:insert(Button)
+
+
+end
+
+
+-- show()
+function scene:show( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is still off screen (but is about to come on screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs when the scene is entirely on screen
+
+	end
+end
+
+
+-- hide()
+function scene:hide( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is on screen (but is about to go off screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs immediately after the scene goes entirely off screen
+
+	end
+end
+
+
+-- destroy()
+function scene:destroy( event )
+
+	local sceneGroup = self.view
+	-- Code here runs prior to the removal of scene's view
+
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+
+return scene
