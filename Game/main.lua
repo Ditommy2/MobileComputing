b9d28232999e42a838286b01067e1dd82d89d91b
Ditommy98 <Ditommy98@users.noreply.github.com>
2020-04-29 16:24:41 +0200

    ditommy

--- Game/main.lua
+++ Game/main.lua
@@ -4,5 +4,10 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
---prova git
+local composer = require( "composer" )
+
+-- Hide status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- Go to the Stanza1 screen
+composer.gotoScene( "stanza1" )
